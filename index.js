f675091761aaecfc9539b3434ac73fcc8976db38
omarmokhtar01 <91415360+omarmokhtar01@users.noreply.github.com>
2022-08-15 16:18:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+/*const fs=require("fs");
+fs.copyFileSync("file.txt","file2.txt");
+*/
+
+/*
+const superheroes = require('superheroes');
+superheroes.all;
+var s= superheroes.random();
+console.log(s);
+*/
+
+//Challenge
+/*const supervillains = require('supervillains');
+supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+var sv=supervillains.random();
+console.log(sv);*/
